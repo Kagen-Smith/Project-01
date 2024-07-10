0267204813994cab0778e3b1a99d7432ce4e07b7
unknown <kirkjholt@gmail.com>
2024-07-09 21:10:32 -0600

    finshed with sign in section

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,26 @@
 const userNameInput = document.querySelector('#userName');
-    const passwordInput = document.querySelector("#password");
-    const submitBtn = document.querySelector('#submitBtn');
+const passwordInput = document.querySelector("#password");
+const submitBtn = document.querySelector('#submitBtn');
 
-    submitBtn.addEventListener('click', function (event) {
-      event.preventDefault();
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
 
-      const user = {
-        userName: userNameInput.value.trim(),
-        password: passwordInput.value.trim(),
-      };
-      console.log("meow")
+  const user = {
+    userName: userNameInput.value.trim(),
+    password: passwordInput.value.trim(),
+  };
 
-      localStorage.setItem('user', JSON.stringify(user));
-    });
+  localStorage.setItem('user', JSON.stringify(user));
 
-    function updateUIWithUsername() {
-      const user = JSON.parse(localStorage.getItem('user'));
-      if (user && user.userName) {
-        const signInBox = document.querySelector('.signInBox');
-        signInBox.innerHTML = `<p>Welcome, ${user.userName}!</p>`;
-      }
-    }
+  location.reload();
+});
 
-    updateUIWithUsername();
+function updateUIWithUsername() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user && user.userName) {
+    const signInBox = document.querySelector('.signInBox');
+    signInBox.innerHTML = `<p>Welcome, ${user.userName}!</p>`;
+  }
+}
+
+updateUIWithUsername();
