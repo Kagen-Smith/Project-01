2b673f72255e9283633db3a32b320dfa336e9669
unknown <kirkjholt@gmail.com>
2024-07-09 20:55:33 -0600

    created js for sign in box

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
-function updateUserName() {
-    addEventListener('click')
-    let username=document.getElementById("userName").value;
-    document.getElementById("userName").innerText = "userName: " + username;
-}
+const userNameInput = document.querySelector('#userName');
+    const passwordInput = document.querySelector("#password");
+    const submitBtn = document.querySelector('#submitBtn');
+
+    submitBtn.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const user = {
+        userName: userNameInput.value.trim(),
+        password: passwordInput.value.trim(),
+      };
+      console.log("meow")
+
+      localStorage.setItem('user', JSON.stringify(user));
+    });
+
+    function updateUIWithUsername() {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user && user.userName) {
+        const signInBox = document.querySelector('.signInBox');
+        signInBox.innerHTML = `<p>Welcome, ${user.userName}!</p>`;
+      }
+    }
+
+    updateUIWithUsername();
