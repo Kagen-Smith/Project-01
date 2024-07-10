550ef99e96c4ae705bef10bb824e67e2f6f83f0f
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-07-09 22:36:21 -0600

    created a function to add the song array to the options list for the datalist option

--- assets/script.js
+++ assets/script.js
@@ -924,30 +924,31 @@ let songs =
     img_url: "http://fireflygrove.com/songnotes/images/artists/DeVotchKa.jpg"
   }
 ]
-
-const song = document.getElementById('song');
-const band = document.getElementById('band');
-const genre = document.getElementById('genre');
-const saveButton = document.getElementById('save');
-
-function savePlaylist() {
-  const playlist = {
-    song: song.value,
-    band: band.value,
-    genre: genre.value
-  };
-  localStorage.setItem('playlist', JSON.stringify(playlist));
+const displaySongOptions = function (songs) {
+  const playlistItem = document.querySelector('#songs');
 }
 
-function renderPlaylist() {
-  const lastPlaylist = JSON.parse(localStorage.getItem('playlist'));
+// const song = document.getElementById('song');
+// const band = document.getElementById('band');
+// const genre = document.getElementById('genre');
+// const saveButton = document.getElementById('save');
 
-  if (lastPlaylist !== null) {
-    document.getElementById('saved-song').innerHTML = lastPlaylist.song;
-    document.getElementById('saved-band').innerHTML = lastPlaylist.band;
-    document.getElementById('saved-genre').innerHTML = lastPlaylist.genre;
-  } else {
-    return;
-  }
-}
+// function savePlaylist() {
+//   const playlist = {
+//     song: song.value,
+//   };
+//   localStorage.setItem('playlist', JSON.stringify(playlist));
+// }
+
+// function renderPlaylist() {
+//   const lastPlaylist = JSON.parse(localStorage.getItem('playlist'));
+
+//   if (lastPlaylist !== null) {
+//     document.getElementById('saved-song').innerHTML = lastPlaylist.song;
+//     document.getElementById('saved-band').innerHTML = lastPlaylist.band;
+//     document.getElementById('saved-genre').innerHTML = lastPlaylist.genre;
+//   } else {
+//     return;
+//   }
+// }
 
